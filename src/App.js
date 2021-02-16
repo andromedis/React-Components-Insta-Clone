7f0376e785acff39a95c60be33c23b47d4e69476
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-02-16 20:34:20 +0000

    Posts.js tasks complete

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
       <SearchBar />
-      <Posts posts={posts} likePost={likePost}/>s
+      <Posts posts={posts} likePost={likePost}/>
     </div>
   );
 };
